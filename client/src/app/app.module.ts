117b593e50c1432b709b1c67a7afc3bee2dbbac8
Ioannis Raptis <Ioannis.Raptis-CIC.Netherlands@ibm.com>
2019-05-23 23:57:21 +0200

    Added header,footer and bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { CampgroundsComponent } from './components/campgrounds/campgrounds.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    CampgroundsComponent
+    CampgroundsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
